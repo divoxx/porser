a1a505f1d1b0512c16fa3ac59025ac3ab1e86736
Rodrigo Kochenburger <divoxx@gmail.com>
2009-09-28 18:25:54 -0300

    Update script that cleanup corpus

--- scripts/bosque_cleanup.rb
+++ scripts/bosque_cleanup.rb
@@ -1,49 +1,61 @@
-in_comment   = false
-in_tag       = false
-in_frase     = false
-substr       = ""
+#!/usr/bin/env ruby
 
-while sentence = $stdin.read(4096)
-  sentence.each_byte do |byte|
-    if in_comment
-      if byte == 10 then
-        substr = ""
-        in_comment = false
-      end
-      next
+unless RUBY_VERSION =~ /^1.9/
+  puts "Ruby >= 1.9 is required"
+  exit
+end
+
+if ARGV.length != 2
+  puts "Usage: #{$0} <original> <output>"
+  exit
+end
+
+in_comment = false
+in_tag     = false
+in_frase   = false
+substr     = ""
+original   = File.open(ARGV[0], "r:ISO-8859-1")
+output     = File.open(ARGV[1], "w:UTF-8")
+
+original.each_char do |char|
+  if in_comment
+    if char == "\n" then
+      substr = ""
+      in_comment = false
     end
+    next
+  end
 
-    if byte == 32 && (substr[-1] == 32 || substr[-1] == 10)
-      next
-    else
-      substr += (case byte when 10 then 32 when 45 then 95 else byte end).chr
-    end
+  if char == " " && (substr[-1] == " " || substr[-1] == "\n")
+    next
+  else
+    substr += (case char when "\n" then " " when "-" then "_" else char end).chr
+  end
 
-    case substr
-    when /\A(.*)#\z/m then
-      in_comment = true
-      print $1
-      substr = ""
+  case substr
+  when /\A(.*)#\z/m then
+    in_comment = true
+    output.write $1
+    substr = ""
 
-    when /\A(.*)\(FRASE[^\(]+\(\z/m then
-      print "#{$1}\n\(S "
-      substr = "("
-    
-    when /\A(.*)\((?!FRASE)([^\s]+)\s\z/m then
-      print "#{$1}("
-      print $2.include?(":") ? $2.split(":")[1].upcase.gsub(/[^\w]+/, '') : $1
-      print " "
-      substr = ""
+  when /\A(.*)\(FRASE[^\(]+\(\z/m then
+    output.write "#{$1}\n\(S "
+    substr = "("
 
-    when /\A(.*)\(([^\s]+)\)\z/m then
-      print "#{$1}(#{$2} #{$2})"
-      substr = ""
+  when /\A(.*)\((?!FRASE)([^\s]+)\s\z/m then
+    output.write "#{$1}("
+    output.write $2.include?(":") ? $2.split(":")[1].upcase.gsub(/[^a-zA-Z_]+/, '').gsub(/_$/, '') : $2
+    output.write " "
+    substr = ""
 
-    when /\A(.*?)[\+_]?\)\z/m then
-      print "#{$1})"
-      substr = ""
-    end
+  when /\A(.*)\(([^\s]+)\)\z/m then
+    output.write "#{$1}(#{$2} #{$2})"
+    substr = ""
+
+  when /\A(.*?)[\+_]?\)\z/m then
+    output.write "#{$1})"
+    substr = ""
   end
 end
 
-print substr
+output.write substr
